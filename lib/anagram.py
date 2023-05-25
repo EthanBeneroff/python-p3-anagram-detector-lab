9f8724235368477294ebe4cb19b628ecae5223d0
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-05-25 14:38:50 -0400

    compltes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list = []):
+        self.list = list
+        new_list = []
+        for item in list:
+            if sorted(self.word) == sorted(item):
+                new_list.append(item)
+            else:
+                return new_list
